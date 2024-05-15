2796b3796bc44d810c83cbea9941d18077e8a8ec
kiwi142857 <779490624@qq.com>
2024-05-15 20:51:29 +0800

    add security config

--- src/main/java/com/web/bookstore/controller/AuthController.java
+++ src/main/java/com/web/bookstore/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.naming.AuthenticationException;
@@ -56,7 +57,7 @@ public class AuthController {
         }
     }
 
-    @PostMapping("/logout")
+    @PutMapping("/logout")
     public ResponseEntity<Object> logout(HttpServletResponse response) {
         Cookie cookie = new Cookie("token", "");
         cookie.setMaxAge(0);
