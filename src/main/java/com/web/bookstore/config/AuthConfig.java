a015b31ccb780d081c997af073936ca35a97f40d
kiwi142857 <779490624@qq.com>
2024-06-08 13:51:58 +0800

    add check in /api/administrator

--- src/main/java/com/web/bookstore/config/AuthConfig.java
+++ src/main/java/com/web/bookstore/config/AuthConfig.java
@@ -1,6 +1,7 @@
 package com.web.bookstore.config;
 
 import com.web.bookstore.interceptor.AuthHandlerInterceptor;
+import com.web.bookstore.interceptor.AdminHandlerInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -12,10 +13,16 @@ public class AuthConfig implements WebMvcConfigurer {
     @Autowired
     AuthHandlerInterceptor authHandlerInterceptor;
 
+    @Autowired
+    AdminHandlerInterceptor adminHandlerInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authHandlerInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/api/auth/*");
+        registry.addInterceptor(adminHandlerInterceptor)
+                .addPathPatterns("/api/administrator/*");
     }
+
 }
