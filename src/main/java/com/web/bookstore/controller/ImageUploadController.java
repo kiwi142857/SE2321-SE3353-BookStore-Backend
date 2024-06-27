62e6d79a916e6c8ee7bdce39fc9288fec92e3980
kiwi142857 <779490624@qq.com>
2024-06-27 21:01:22 +0800

    完成用户profile更新

--- src/main/java/com/web/bookstore/controller/ImageUploadController.java
+++ src/main/java/com/web/bookstore/controller/ImageUploadController.java
@@ -1,20 +1,34 @@
 package com.web.bookstore.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.web.bookstore.dto.ImageUpLoadDTO;
+import com.web.bookstore.exception.UserBannedException;
+import com.web.bookstore.model.User;
 import com.web.bookstore.service.CosService;
+import com.web.bookstore.service.UserService;
+import com.web.bookstore.util.SessionUtils;
 
 @RestController
+@RequestMapping("/api/image")
 public class ImageUploadController {
 
-    @Autowired
     private CosService cosService;
 
-    @PostMapping(value = "/uploadImage", consumes = { "multipart/form-data" })
+    private UserService userService;
+
+    public ImageUploadController(CosService cosService, UserService userService) {
+        this.cosService = cosService;
+        this.userService = userService;
+    }
+
+    @PostMapping(value = "/upload", consumes = { "multipart/form-data" })
     public String uploadImage(@RequestPart("image") MultipartFile image) {
         try {
             String url = cosService.uploadImage(image);
@@ -23,6 +37,58 @@ public class ImageUploadController {
         } catch (Exception e) {
             e.printStackTrace();
             return "Upload failed";
+        }
+    }
+
+    @PostMapping(value = "/user/profile", consumes = { "multipart/form-data" })
+    public ResponseEntity<Object> uploadUserProfile(@RequestPart("image") MultipartFile image) {
+        try {
+            User sessionUser = SessionUtils.getUser();
+            if (sessionUser == null) {
+                throw new Exception("User not logged in");
+            }
+            User user = userService.findUserById(sessionUser.getId());
+            if (user.getStatus() == 1) {
+                throw new UserBannedException("您的账号已被禁用");
+            }
+            String url = cosService.uploadImage(image);
+            // 这里可以将URL保存到数据库中
+            user.setAvatar(url);
+            userService.save(user);
+            ImageUpLoadDTO imageUpLoadDTO = new ImageUpLoadDTO();
+            imageUpLoadDTO.setImageUrl(url);
+            return ResponseEntity.ok(imageUpLoadDTO);
+        } catch (UserBannedException e) {
+            return ResponseEntity.status(403).body("User banned");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body("Upload failed");
+        }
+    }
+
+    @PostMapping(value = "/book/{id}/cover", consumes = { "multipart/form-data" })
+    public ResponseEntity<Object> uploadBookProfile(@RequestPart("image") MultipartFile image) {
+        try {
+            User sessionUser = SessionUtils.getUser();
+            if (sessionUser == null) {
+                throw new Exception("User not logged in");
+            }
+            User user = userService.findUserById(sessionUser.getId());
+            if (user.getStatus() == 1) {
+                throw new UserBannedException("您的账号已被禁用");
+            }
+            String url = cosService.uploadImage(image);
+            // 这里可以将URL保存到数据库中
+            user.setAvatar(url);
+            userService.save(user);
+            ImageUpLoadDTO imageUpLoadDTO = new ImageUpLoadDTO();
+            imageUpLoadDTO.setImageUrl(url);
+            return ResponseEntity.ok(imageUpLoadDTO);
+        } catch (UserBannedException e) {
+            return ResponseEntity.status(403).body("User banned");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body("Upload failed");
         }
     }
 }
---
+++ src/main/java/com/web/bookstore/dto/ImageUpLoadDTO.java
@@ -0,0 +1,8 @@
+package com.web.bookstore.dto;
+
+import lombok.Data;
+
+@Data
+public class ImageUpLoadDTO {
+    private String imageUrl;
+}
